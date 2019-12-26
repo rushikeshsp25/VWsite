366850154197476483ecc15041c04c635cdd54f1
Ubuntu <ubuntu@ip-172-31-36-33.ap-south-1.compute.internal>
2019-12-26 17:06:25 +0000

    side

--- VWsite/settings.py
+++ VWsite/settings.py
@@ -126,7 +126,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'visionware.pune@gmail.com'
-EMAIL_HOST_PASSWORD = 'Raje@7845'
+EMAIL_HOST_PASSWORD = 'VisionWare@2020'
 EMAIL_PORT = 587
 
 LOGIN_URL = "/login/"
